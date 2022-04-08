badbde3a6e8ac220c500e104d2d62dd8dafd34dd
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2022-04-08 11:49:48 +0200

    with C-d buffers can be closed in the buffer list

--- lua/telescope-nvim.lua
+++ lua/telescope-nvim.lua
@@ -45,6 +45,17 @@ require("telescope").setup {
   pickers = {
     find_files = {
       hidden = true
+    },
+    buffers = {
+      show_all_buffers = true,
+      sort_lastused = true,
+      -- theme = "dropdown",
+      -- previewer = false,
+      mappings = {
+        i = {
+          ["<c-d>"] = "delete_buffer",
+        }
+      }
     }
   },
   extensions = {
