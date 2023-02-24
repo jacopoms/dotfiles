f83817af01df01b68afa486d3b35906050b98e66
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2023-02-24 10:06:55 +0100

    formatting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,174 +1,180 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {
-      { 'nvim-lua/popup.nvim' },
-      { 'nvim-lua/plenary.nvim' },
-      { 'nvim-telescope/telescope-live-grep-args.nvim' },
-      { "nvim-telescope/telescope-github.nvim" },
-      { "LinArcX/telescope-env.nvim" },
-    }
-  }
-  use {
-    'sudormrfbin/cheatsheet.nvim',
+return require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = {
+			{ "nvim-lua/popup.nvim" },
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-live-grep-args.nvim" },
+			{ "nvim-telescope/telescope-github.nvim" },
+			{ "LinArcX/telescope-env.nvim" },
+		},
+	})
+	use({
+		"sudormrfbin/cheatsheet.nvim",
 
-    requires = {
-      { 'nvim-telescope/telescope.nvim' },
-      { 'nvim-lua/popup.nvim' },
-      { 'nvim-lua/plenary.nvim' },
-    }
-  }
-  use 'famiu/nvim-reload'
-  use { 'nvim-telescope/telescope-file-browser.nvim' }
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use 'neovim/nvim-lspconfig'
-  use {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    requires = 'neovim/nvim-lspconfig'
-  }
-  use 'mhartington/formatter.nvim'
-  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  -- use 'hrsh7th/cmp-vsnip'
-  -- use 'hrsh7th/vim-vsnip'
-  -- use 'hrsh7th/vim-vsnip-integ'
-  use {
-  	"L3MON4D3/LuaSnip",
-    -- follow latest release.
-    tag = "v<CurrentMajor>.*",
-    -- install jsregexp (optional!:).
-    run = "make install_jsregexp"
-  }
-  use {
-    'tzachar/cmp-tabnine',
-    run = './install.sh',
-    requires = {
-      'hrsh7th/nvim-cmp'
-    }
-  }
-  use {
-    'onsails/lspkind.nvim',
-    requires = {
-      'hrsh7th/nvim-cmp'
-    }
-  }
-  use { 'folke/which-key.nvim' }
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-  use { 'ziontee113/syntax-tree-surfer' }
-  use {
-    'akinsho/nvim-bufferline.lua',
-    requires = 'kyazdani42/nvim-web-devicons'
-  }
-  use {
-    "danymat/neogen",
-    requires = {
-      "nvim-treesitter/nvim-treesitter",
-      "hrsh7th/vim-vsnip"
-    },
-    -- Uncomment next line if you want to follow only stable versions
-    tag = "*"
-  }
-  use {
-    'rmagatti/goto-preview',
-    config = function()
-      require('goto-preview').setup { default_mappings = true}
-    end
-  }
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = 'kyazdani42/nvim-web-devicons'
-  }
-  use { "lewis6991/hover.nvim" }
-  -- use 'glepnir/lspsaga.nvim'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-rhubarb' -- open file on github
-  use 'RRethy/nvim-treesitter-endwise'
-  use 'ryanoasis/vim-devicons'
-  use 'machakann/vim-highlightedyank'
-  use 'vim-ruby/vim-ruby'
-  use {
-    'ruifm/gitlinker.nvim',
-    requires = 'nvim-lua/plenary.nvim',
-  }
-  use { 'akinsho/git-conflict.nvim', tag = "*", config = function()
-    require('git-conflict').setup()
-  end }
-  use 'elixir-editors/vim-elixir'
-  use 'lukas-reineke/indent-blankline.nvim'
-  use { 'simrat39/symbols-outline.nvim' }
-  use {
-    'windwp/nvim-spectre',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    }
-  }
-  -- ' color schemes
-  -- use 'EdenEast/nightfox.nvim'
-  use 'navarasu/onedark.nvim'
-  use 'shaunsingh/solarized.nvim'
-  -- use 'folke/tokyonight.nvim'
-  use 'projekt0n/github-nvim-theme'
-  use 'tiagovla/tokyodark.nvim'
-  use 'ray-x/aurora'
-  use 'lifepillar/vim-solarized8'
-  -- use 'sainnhe/sonokai'
-  use 'marko-cerovac/material.nvim'
-  use 'https://gitlab.com/__tpb/monokai-pro.nvim'
-  use 'yorik1984/newpaper.nvim'
+		requires = {
+			{ "nvim-telescope/telescope.nvim" },
+			{ "nvim-lua/popup.nvim" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+	})
+	use("famiu/nvim-reload")
+	use({ "nvim-telescope/telescope-file-browser.nvim" })
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use("neovim/nvim-lspconfig")
+	use({
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		requires = "neovim/nvim-lspconfig",
+	})
+	use("mhartington/formatter.nvim")
+	use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-cmdline")
+	-- use 'hrsh7th/cmp-vsnip'
+	-- use 'hrsh7th/vim-vsnip'
+	-- use 'hrsh7th/vim-vsnip-integ'
+	use({
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		tag = "v<CurrentMajor>.*",
+		-- install jsregexp (optional!:).
+		run = "make install_jsregexp",
+	})
+	use({
+		"tzachar/cmp-tabnine",
+		run = "./install.sh",
+		requires = {
+			"hrsh7th/nvim-cmp",
+		},
+	})
+	use({
+		"onsails/lspkind.nvim",
+		requires = {
+			"hrsh7th/nvim-cmp",
+		},
+	})
+	use({ "folke/which-key.nvim" })
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use({ "ziontee113/syntax-tree-surfer" })
+	use({
+		"akinsho/nvim-bufferline.lua",
+		requires = "kyazdani42/nvim-web-devicons",
+	})
+	use({
+		"danymat/neogen",
+		requires = {
+			"nvim-treesitter/nvim-treesitter",
+			"hrsh7th/vim-vsnip",
+		},
+		-- Uncomment next line if you want to follow only stable versions
+		tag = "*",
+	})
+	use({
+		"rmagatti/goto-preview",
+		config = function()
+			require("goto-preview").setup({ default_mappings = true })
+		end,
+	})
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = "kyazdani42/nvim-web-devicons",
+	})
+	use({ "lewis6991/hover.nvim" })
+	-- use 'glepnir/lspsaga.nvim'
+	-- use("tpope/vim-fugitive")
+	use("tpope/vim-rhubarb") -- open file on github
+	use("RRethy/nvim-treesitter-endwise")
+	use("ryanoasis/vim-devicons")
+	use("machakann/vim-highlightedyank")
+	use("vim-ruby/vim-ruby")
+	use({
+		"ruifm/gitlinker.nvim",
+		requires = "nvim-lua/plenary.nvim",
+	})
+	use({
+		"akinsho/git-conflict.nvim",
+		tag = "*",
+		config = function()
+			require("git-conflict").setup()
+		end,
+	})
+	use("elixir-editors/vim-elixir")
+	use("lukas-reineke/indent-blankline.nvim")
+	use({ "simrat39/symbols-outline.nvim" })
+	use({
+		"windwp/nvim-spectre",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+	})
+	-- ' color schemes
+	-- use 'EdenEast/nightfox.nvim'
+	use("navarasu/onedark.nvim")
+	use("shaunsingh/solarized.nvim")
+	-- use 'folke/tokyonight.nvim'
+	use("projekt0n/github-nvim-theme")
+	use("tiagovla/tokyodark.nvim")
+	use("ray-x/aurora")
+	use("lifepillar/vim-solarized8")
+	-- use 'sainnhe/sonokai'
+	use("marko-cerovac/material.nvim")
+	use("https://gitlab.com/__tpb/monokai-pro.nvim")
+	use("yorik1984/newpaper.nvim")
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use 'cwebster2/github-coauthors.nvim'
-  use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end
-  }
-  use 'vim-test/vim-test'
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = { 'nvim-lua/plenary.nvim' }
-  }
-  use 'norcalli/nvim-colorizer.lua'
-  use {
-	"windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
-  }
-  use {
-    'goolord/alpha-nvim',
-    requires = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
-  }
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+	use("cwebster2/github-coauthors.nvim")
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
+	use("vim-test/vim-test")
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+	use("norcalli/nvim-colorizer.lua")
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
+	use({
+		"goolord/alpha-nvim",
+		requires = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	})
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
