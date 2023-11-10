b9c07e4e8012170a43deae92ef62615c5547caa1
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2023-11-10 10:42:58 +0100

    updated config

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -8,6 +8,24 @@
 return {
   { import = "lazyvim.plugins.extras.ui.mini-animate" },
   { import = "lazyvim.plugins.extras.coding.copilot" },
+  { import = "lazyvim.plugins.extras.editor.symbols-outline" },
   { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
   { import = "lazyvim.plugins.extras.lang.json" },
+
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+        ruby = true,
+      },
+      copilot_node_command = "/Users/jacopo/.asdf/installs/nodejs/21.1.0/bin/node",
+    },
+  },
 }
