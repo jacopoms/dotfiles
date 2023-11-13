74674e2b190b44f57f0d473854fff82589c556c5
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2023-11-13 10:22:24 +0100

    remvoed keymap"

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Save with leader W
-vim.api.nvim_set_keymap("n", ";w", ":w<CR>", { noremap = true })
 -- Search forward for word under the cursor
 vim.api.nvim_set_keymap("n", "<leader>h", "/<C-r><C-w><CR>", { noremap = true })
 -- Search backward for word under the cursor
