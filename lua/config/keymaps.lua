bfc199e80f590abc9a233d0a3d6acf840be00bfc
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2023-11-13 16:40:17 +0100

    fix keymaps conflict

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- Search forward for word under the cursor
-vim.api.nvim_set_keymap("n", "<leader>h", "/<C-r><C-w><CR>", { noremap = true })
--- Search backward for word under the cursor
-vim.api.nvim_set_keymap("n", "<leader>hh", "?<C-r><C-w><CR>", { noremap = true })
