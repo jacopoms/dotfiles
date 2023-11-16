920dd82d267e1c0d1746dbde5101a4a824b24790
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2023-11-16 10:05:22 +0100

    improve cmp and copilot

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,9 +8,12 @@ return {
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     local has_words_before = function()
+      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+        return false
+      end
       unpack = unpack or table.unpack
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("^%s*$") == nil
     end
 
     local luasnip = require("luasnip")
@@ -20,13 +23,14 @@ return {
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-          cmp.select_next_item()
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
           -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
           -- this way you will only jump inside the snippet region
         elseif luasnip.expand_or_jumpable() then
           luasnip.expand_or_jump()
         elseif has_words_before() then
-          cmp.complete()
+          -- cmp.complete()
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
         else
           fallback()
         end
