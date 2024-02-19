bcb9c0f59bc764d8b724629abe4d4eb4c9b26a96
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2024-02-19 17:06:25 +0100

    enabling spectre plugin for search and replace

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
-  { "nvim-pack/nvim-spectre", enabled = false },
+  -- { "nvim-pack/nvim-spectre", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/neodev.nvim", enabled = false },
 }
