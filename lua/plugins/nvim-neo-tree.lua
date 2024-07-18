d1cb197b22f4e3d29f7e948a3a8eeaa08550817b
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2024-07-18 10:49:17 +0200

    updated config

--- lua/plugins/nvim-neo-tree.lua
+++ lua/plugins/nvim-neo-tree.lua
@@ -5,6 +5,15 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
+    "s1n7ax/nvim-window-picker",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  opts = {
+    window = {
+      mappings = {
+        ["S"] = "split_with_window_picker",
+        ["s"] = "vsplit_with_window_picker",
+      },
+    },
   },
 }
