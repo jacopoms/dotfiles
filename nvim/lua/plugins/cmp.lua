19db9a542cdcb00a7c8bf5d6ba7276b75d51efe1
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2024-08-21 10:30:24 +0200

    updated nvim config

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -4,17 +4,23 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-nvim-lua",
     "hrsh7th/cmp-emoji",
+    "hrsh7th/cmp-nvim-lsp-document-symbol",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "lukas-reineke/cmp-rg",
   },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
-    opts.sources = {
+    table.insert(opts.sources, {
       { name = "copilot" },
       { name = "nvim_lsp" },
       { name = "nvim_lua" },
+      { name = "rg" },
       { name = "path" },
       { name = "luasnip" },
       { name = "emoji" },
+      { name = "nvim_lsp_document_symbol" },
+      { name = "nvim_lsp_signature_help" },
       { name = "buffer" },
-    }
+    })
   end,
 }
