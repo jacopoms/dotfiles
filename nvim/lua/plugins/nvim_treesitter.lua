688895ff8cc80ca32f9c1b0cfa0220166f243429
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2024-09-09 11:38:26 +0200

    updated nvim treesitter objects config

--- nvim/lua/plugins/nvim_treesitter.lua
+++ nvim/lua/plugins/nvim_treesitter.lua
@@ -45,6 +45,16 @@ return {
         enable = true,
       },
       textobjects = {
+        select = {
+          enable = true,
+        },
+        move = {
+          enable = true,
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+        },
         lsp_interop = {
           enable = true,
           border = "none",
