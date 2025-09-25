88609e1191c2a2e75fcf8e05fa941ac31c9578b4
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2025-09-25 15:39:29 +0200

    updating neovim for personal use

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,20 +1,30 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    diagnostics = {
-      float = {
-        source = "always", -- Or "if_many"
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        float = {
+          source = "always", -- Or "if_many"
+        },
+        update_in_insert = true,
+        virtual_text = false,
+        -- virtual_lines = {
+        --   current_line = true,
+        -- },
       },
-      update_in_insert = true,
-      virtual_text = false,
-      -- virtual_lines = {
-      --   current_line = true,
-      -- },
-    },
-    servers = {
-      solargraph = {
-        mason = false,
-        cmd = { "bundle", "exec", "solargraph", "stdio" },
+      servers = {
+        solargraph = {
+          mason = false,
+          cmd = { "bundle", "exec", "solargraph", "stdio" },
+        },
+        rubocop = {
+          mason = false,
+        },
+
+        ruby_lsp = {
+          mason = false,
+          cmd = { vim.fn.expand("~/.asdf/shims/ruby-lsp") },
+        },
       },
       rubocop = {
         mason = false,
