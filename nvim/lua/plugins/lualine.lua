4c5911f75d61e41281d6df65ae9c6075a5f08b87
Jacopo Ghisolfi <jacopo.ghisolfi@marleyspoon.com>
2025-11-11 08:57:57 +0100

    updated config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -21,5 +21,35 @@ return {
     local branch_opts = { "ex.git.branch", max_length = 24, icon = " ", crop = { side = "right" } }
     opts.sections.lualine_b = { branch_opts }
     table.insert(opts.sections.lualine_c, { "navic", color_correction = "dynamic" })
+    -- Copilot status
+    table.insert(opts.sections.lualine_c, {
+      function()
+        return " "
+      end,
+      color = function()
+        local status = require("sidekick.status").get()
+        if status then
+          return status.kind == "Error" and "DiagnosticError" or status.busy and "DiagnosticWarn" or "Special"
+        end
+      end,
+      cond = function()
+        local status = require("sidekick.status")
+        return status.get() ~= nil
+      end,
+    })
+
+    -- CLI session status
+    table.insert(opts.sections.lualine_x, 2, {
+      function()
+        local status = require("sidekick.status").cli()
+        return " " .. (#status > 1 and #status or "")
+      end,
+      cond = function()
+        return #require("sidekick.status").cli() > 0
+      end,
+      color = function()
+        return "Special"
+      end,
+    })
   end,
 }
